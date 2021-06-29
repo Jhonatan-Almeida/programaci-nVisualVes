e7ba3bdf63b64af29e442644081f37bfd2c2c4a2
Jhonatan-Almeida <jhonalmeida@hotmail.com>
2021-06-29 15:20:27 -0500

    Otro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "Bienvenidos al nuevo curso de programación visual 2021";
+
